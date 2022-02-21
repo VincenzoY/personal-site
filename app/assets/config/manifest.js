3a2ce6e3212a944bb62383c2a638ffb516ac513c
Vincent Yee <vincenzoyee@gmail.com>
2022-02-21 16:39:40 -0700

    fixed manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,16 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-
-let pages = document.querySelectorAll(".pages > a");
-let account = document.querySelector(".account");
-let hamburger = document.querySelector("#hamburger");
-
-hamburger.addEventListener("click", function () {
-    console.log("test");
-    for (let i = 0; i < pages.length; i++) {
-        pages[i].classList.toggle("hidden");
-    }
-    //pages.classList.toggle("hidden");
-    account.classList.toggle("hidden");
-});
